535b628602309f43804604c91821b99d60e171fd
bizzy1227 <narkoman1227@gmail.com>
2019-10-02 16:50:09 +0300

    Добавлен файл index.js в котором новые данные записываются в новую строку файла text.txt, а так же отправляет сообщение на почту

--- 13/index.js
+++ 13/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
 
+let text = fs.readFileSync("text.txt", "utf8");
+
+
+let addNext = "2) Second line."
+
+fs.appendFileSync("text.txt", '\r\n'+addNext);
+
+
+
+//send Email
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'jackjs194040@gmail.com',
+    pass: 'leonardo4040'
+  }
+});
+
+var mailOptions = {
+  from: 'jackjs194040@gmail.com',
+  to: 'odrytoty@ukr.net',
+  subject: 'nodemailer test',
+  text: 'Hello world'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
