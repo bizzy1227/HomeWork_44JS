e4afb5a0179c575f447a4ae990d817bf3c2d7e10
bizzy1227 <narkoman1227@gmail.com>
2019-10-09 19:54:46 +0300

    доработал подключение JSON privatbanka

--- 14/index.js
+++ 14/index.js
@@ -1,9 +1,19 @@
 const http = require('http'); //подключаем модули http and fs
 const fs = require('fs');
+const request = require('request');//подключили модуль для запросов(предварительно надо установить его)
+
 
 const page = fs.readFileSync('index.html');
-
+//запустили сервер
 http.createServer(function(req, res){
+    //отправляем запрос на сторонний API
+     request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR2LL3R7yJOveApsLk7p7u9mi_n8CCYVV8LRlGbhxfF64tWwCSKRgJREPC4', function (error, response, body) {
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //Выводим в консоль полученый JSON, предварительно отпарсив его в объект JS 
+  console.log(JSON.parse(body)); // Print the HTML for the Google homepage.
+});
+ 
     //console.log(req);
     //сравниваем значения свойства req.url (запрос по адресной строке)
     if(req.url === '/'){  
