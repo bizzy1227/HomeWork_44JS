828dcd1759e64f49d304fe54d609e37c9b25cf7c
bizzy1227 <narkoman1227@gmail.com>
2019-10-16 18:09:05 +0300

    Добавлена функция test, которая через callback проверяет коректность полученных данных

--- 17/index.js
+++ 17/index.js
@@ -5,20 +5,38 @@ const url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 
 http.createServer(function(req, res){
      request(url, function(error, response, body){
-         //прописываем корректную кодироку
+         //прописываем корректную кодировку
          res.writeHead(200, {
             "Content-Type": "text/html; charset=utf-8"
         });
          let data = JSON.parse(body);
          let buy = data[0].buy;
          let sale = data[0].sale;
+         
+// функция для проверки корректности значений курса через callback
+        function test(data, callback){
+            if(data > 0 && typeof data === 'string'){
+                callback('its okay');
+            } else {
+                callback('error'); 
+            };
+        };
+// если buy больше 0 и тип данных string то в callback передаем 'its okay'
+// иначе 'error'
+        test(buy, function(data){
+            console.log(data);
+        });
+// если sale больше 0 и тип данных string то в callback передаем 'its okay'
+// иначе 'error'
+        test(sale, function(data){
+            console.log(data);
+        });
+
          let saleUSD = new Converter(buy);
          let buyUSD = new Converter(sale);
-         res.write('За 100 долларов Вы получите '+saleUSD.convertToUa(100)+' гривен. ');
-         res.write('За 1000 гривен Вы получите '+buyUSD.convertToUs(1000)+' долларов.');
+         res.write('За 100 долларов Вы получите '+saleUSD.convertToUa(100)+' гривен. '+'За 1000 гривен Вы получите '+buyUSD.convertToUs(1000)+' долларов.');
          res.end();
      }); 
 }).listen(8080, function(){
     console.log('Server at localhost:8080');
 });
-
