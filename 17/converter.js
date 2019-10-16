8ee168289e968ed95f0f1582d73f4613da227c5c
bizzy1227 <narkoman1227@gmail.com>
2019-10-16 15:16:17 +0300

    модуль-конвертер

--- 17/converter.js
+++ 17/converter.js
@@ -1,1 +1,39 @@
+// function Converter(baseCurrencyUs) {
+//     this.baseCurrencyUs = baseCurrencyUs;
+// };
+
+// Converter.prototype.roundTwoDecimal = function(amount){
+//     return Math.round(amount * 100) / 100;
+// };
+
+// Converter.prototype.convertToUa =function(currency){
+//     return this.roundTwoDecimal(currency * this.baseCurrencyUs);
+// };
+
+// Converter.prototype.convertToUs =function(currency){
+//     return this.roundTwoDecimal(currency / this.baseCurrencyUs);
+// };
+
+// module.exports = Converter;
+//----------------------------------------------------
+
+// Переписан на es6 синтаксис
+class Converter{
+    constructor(baseCurrencyUs){
+        this.baseCurrencyUs = baseCurrencyUs;
+    };
+    //helper приводит число к нормальному виду
+    roundTwoDecimal(amount){
+        return Math.round(amount * 100) / 100; 
+    };
+
+    convertToUa(currency){
+        return this.roundTwoDecimal(currency * this.baseCurrencyUs);
+    };
+
+    convertToUs(currency){
+        return this.roundTwoDecimal(currency / this.baseCurrencyUs);
+    };
+};
+module.exports = Converter;
 
