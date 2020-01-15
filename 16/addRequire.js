3b4cf8be215b6b5687655a88c38fa3383c38770a
bizzy1227 <archstar1227@gmail.com>
2020-01-15 21:15:46 +0200

    Update eslint #3

--- 16/addRequire.js
+++ 16/addRequire.js
@@ -1,22 +1,21 @@
 
 const fs = require('fs');
-//подключаем чайл в котором будем смотреть установленные модули
-let pack = require('./package.json');
+// подключаем чайл в котором будем смотреть установленные модули
+const pack = require('./package.json');
 
-//пушим в массив названия установленных модулей 
-let arr = [];
-for(key in pack.dependencies){
-    arr.push(key);
+// пушим в массив названия установленных модулей
+const arr = [];
+for (const key in pack.dependencies) {
+   arr.push(key);
 }
 
-module.exports.moveAllRequire = function(){
-        fs.appendFileSync("index.js", '\r\n'+"//Подключение модулей с помощью модуля addRequire.js");
-        fs.appendFileSync("index.js", '\r\n'+"const fs = require('fs');");
-        fs.appendFileSync("index.js", '\r\n'+"const http = require('http');");
-        fs.appendFileSync("index.js", '\r\n'+"//Подключаем модули которые указаны в package.json с помощью модуля addRequire.js");
-        for(let i=0; i<arr.length; i++){
-            // если в названии модуля встречается "-" заменяем его на "_" для корректного создания переменой (arr[i].replace("-","_"))
-            fs.appendFileSync("index.js", '\r\n'+"const "+arr[i].replace("-","_")+" = require('"+arr[i]+"');");
-            }
-    
-    };
+module.exports.moveAllRequire = function () {
+   fs.appendFileSync('index.js', '\r\n' + '//Подключение модулей с помощью модуля addRequire.js');
+   fs.appendFileSync('index.js', '\r\n' + "const fs = require('fs');");
+   fs.appendFileSync('index.js', '\r\n' + "const http = require('http');");
+   fs.appendFileSync('index.js', '\r\n' + '//Подключаем модули которые указаны в package.json с помощью модуля addRequire.js');
+   for (let i = 0; i < arr.length; i++) {
+   // если в названии модуля встречается "-" заменяем его на "_" для корректного создания переменой (arr[i].replace("-","_"))
+      fs.appendFileSync('index.js', '\r\n' + 'const ' + arr[i].replace('-', '_') + " = require('" + arr[i] + "');");
+   }
+};
--- 16/index.js
+++ 16/index.js
@@ -1,2 +1,2 @@
-let addRequire =require('./addRequire');
+const addRequire = require('./addRequire');
 addRequire.moveAllRequire();
